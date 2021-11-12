b199305251c0b0a147c9ce692bd564e1de09fe53
AmadeoNeto <amadeotatocota@gmail.com>
2021-11-12 15:54:55 -0300

    Show Windows

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
+import ui.AddSongWindow;
+import ui.PlayerWindow;
+
+import java.awt.event.*;
+
 public class Main {
     public static void main(String[] args) {
+        Player player = new Player();
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,98 @@
-public class Player {
-    public Player() {
-    }
-}
+import ui.AddSongWindow;
+import ui.PlayerWindow;
 
+import java.awt.event.*;
+
+public class Player {
+
+    WindowListener windowListener;
+
+    public Player() {
+
+        PlayerWindow window = new PlayerWindow(
+                playButton,
+                placeholderListener,
+                addSongButton,
+                placeholderListener,
+                placeholderListener,
+                placeholderListener,
+                placeholderListener,
+                placeholderListener,
+                placeholderListener,
+                mouseListener,
+                motionListener,
+                "CIn Media Player",
+                new String[5][4]
+        );
+
+        windowListener = window.getAddSongWindowListener();
+    }
+
+    // Listeners
+    ActionListener addSongOk = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Musica adionada");
+        }
+    };
+
+    ActionListener addSongButton = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            AddSongWindow addWindow = new AddSongWindow("465465", addSongOk, windowListener);
+        }
+    };
+
+    ActionListener placeholderListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    };
+
+    ActionListener playButton = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("hello world");
+        }
+    };
+
+    MouseListener mouseListener = new MouseListener() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+    };
+
+    MouseMotionListener motionListener = new MouseMotionListener() {
+        @Override
+        public void mouseDragged(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+
+        }
+    };
+}
