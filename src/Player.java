824259ecaa3c73b5cfbdde5a6999bc3dcdc7c18e
AmadeoNeto <amadeotatocota@gmail.com>
2021-11-13 23:16:41 -0300

    Play, pause and stop

--- src/Player.java
+++ src/Player.java
@@ -2,22 +2,70 @@ import ui.*;
 
 import java.awt.event.*;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
 public class Player {
 
-    PlayerWindow window;
-    WindowListener windowListener;
-    AddSongWindow addWindow;
+    private PlayerWindow window;
+    private WindowListener windowListener;
+    private AddSongWindow addWindow;
+
+    private String[] currentSong = null;
+    private Boolean isPlaying = false;
+    private boolean isActive = false;
+    private boolean isRepeat = false;
+    private int currentTime = 0;
     private int numberSongs = 0;
 
     ArrayList<String[]> queue = new ArrayList<>();
-    //String[][] queue = new String[10][7];
+
+    Thread windowsUpdater;
+    Thread timerUpdater;
+    CyclicBarrier timerBarrier;
+
+    Runnable increaseTimer = () ->{
+        try {
+            int totalTime = Integer.parseInt(currentSong[5]);
+            while(currentTime <= totalTime) {
+                Thread.sleep(1000);
+                timerBarrier.await();
+                currentTime++;
+            }
+        } catch (InterruptedException | BrokenBarrierException e) {
+
+        }
+    };
+
+    Runnable updateWindow = () -> {
+        int songID = Integer.parseInt(currentSong[6]);
+        int totalTime = Integer.parseInt(currentSong[5]);
+
+        while(currentTime <= totalTime) {
+            try {
+                int await = timerBarrier.await();
+
+                if (await == 0) {
+                    window.updateMiniplayer(
+                            isActive,
+                            isPlaying,
+                            isRepeat,
+                            currentTime,
+                            totalTime,
+                            songID,
+                            queue.size()
+                    );
+                }
+            } catch (Exception e) {
+
+            }
+        }
+    };
 
     public Player() {
 
         window = new PlayerWindow(
-                playButton,
+                playNowListener,
                 buttonListenerRemove,
                 addSongButton,
                 placeholderListener,
@@ -29,18 +77,29 @@ public class Player {
                 mouseListener,
                 motionListener,
                 "CIn Media Player",
-                //queue
                 getQueueArray()
         );
 
         windowListener = window.getAddSongWindowListener();
-
     }
 
+    /**
+     * @return the queue as a array of songs (String[])
+     */
     public String[][] getQueueArray() {
         String[][] array = new String[queue.size()][7];
         array = (String[][]) queue.toArray(array);
         return array;
+    }
+
+    public String[] getMusicByID(int songID){
+
+        for (String[] song : queue) {
+            if (song[6].equals("" + songID)) {
+                return song;
+            }
+        }
+        return null;
     }
 
     // Listeners
@@ -85,10 +144,34 @@ public class Player {
         }
     };
 
-    ActionListener playButton = new ActionListener() {
+    ActionListener playNowListener = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            System.out.println("hello world");
+            isPlaying = true;
+            isActive = true;
+            window.updatePlayPauseButton(true);
+
+            int songID = window.getSelectedSongID();
+            currentSong = getMusicByID(songID);
+            currentTime = 0;
+
+            window.updateMiniplayer(
+                    true,
+                    true,
+                    isRepeat,
+                    currentTime,
+                    Integer.parseInt(currentSong[5]),
+                    songID,
+                    queue.size());
+
+            window.enableScrubberArea();
+
+            windowsUpdater = new Thread(updateWindow);
+            timerUpdater = new Thread(increaseTimer);
+
+            timerBarrier = new CyclicBarrier(1);
+            windowsUpdater.start();
+            timerUpdater.start();
         }
     };
 
---
+++ src/UpdatePlayer.java
@@ -0,0 +1,15 @@
+import ui.*;
+
+public class UpdatePlayer extends Thread{
+
+    PlayerWindow window;
+
+    public UpdatePlayer(PlayerWindow window){
+        this.window = window;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
